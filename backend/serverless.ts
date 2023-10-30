0e7bde451d8f344e4980f66fefd5a7f5987c57ed
Jean Santos <jeanwfsantos@gmail.com>
2023-10-30 10:03:31 +0000

    add gateway response default 4xx resource

--- backend/serverless.ts
+++ backend/serverless.ts
@@ -98,6 +98,22 @@ const serverlessConfiguration: AWS = {
   },
   resources: {
     Resources: {
+      GatewayResponseDefault4XX: {
+        Type: 'AWS::ApiGateway::GatewayResponse',
+        Properties: {
+          ResponseParameters: {
+            'gatewayresponse.header.Access-Control-Allow-Origin': "'*'",
+            'gatewayresponse.header.Access-Control-Allow-Headers':
+              "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+            'gatewayresponse.header.Access-Control-Allow-Methods':
+              "'GET,OPTIONS,POST'",
+          },
+          ResponseType: 'DEFAULT_4XX',
+          RestApiId: {
+            Ref: 'ApiGatewayRestApi',
+          },
+        },
+      },
       ChannelsDynamoDBTable: {
         Type: 'AWS::DynamoDB::Table',
         Properties: {

645ba9fb59d89f448552394c9603a34f789cf809
Jean Santos <jeanwfsantos@gmail.com>
2023-11-03 09:01:52 +0000

    add channel table list

--- frontend/src/app/pages/channels/channels.component.ts
+++ frontend/src/app/pages/channels/channels.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import {
   FormControl,
   NonNullableFormBuilder,
@@ -8,13 +8,36 @@ import {
   Validators,
 } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
-import { EMPTY, catchError } from 'rxjs';
+import { EMPTY, Observable, catchError, map, of } from 'rxjs';
 
 import { environment } from '@env';
+
+interface User {
+  sub: string;
+  email_verified: boolean;
+  updated_at: string;
+  nickname: string;
+  name: string;
+  given_name: string;
+  locale: string;
+  family_name: string;
+  picture: string;
+  email: string;
+}
+
+interface Channel {
+  createdAt: string;
+  user: User;
+  channelId: string;
+  name: string;
+  userId: string;
+  attachmentUrl?: string;
+}
 
 @Component({
   selector: 'app-channels',
@@ -27,16 +50,25 @@ import { environment } from '@env';
     MatTableModule,
     ReactiveFormsModule,
     MatSnackBarModule,
+    MatDividerModule,
   ],
   templateUrl: './channels.component.html',
 })
-export class ChannelsComponent {
+export class ChannelsComponent implements OnInit {
   private fb = inject(NonNullableFormBuilder);
   form = this.fb.group({
     name: new FormControl('', Validators.required),
   });
   private http = inject(HttpClient);
   private snackBar = inject(MatSnackBar);
+  channels$: Observable<Channel[]> = of([]);
+  columnsToDisplay = ['attachmentUrl', 'channelId', 'name', 'edit'];
+
+  ngOnInit(): void {
+    this.channels$ = this.http
+      .get<{ channels: Channel[] }>(`${environment.endpoint}/my-channels`)
+      .pipe(map(response => response.channels));
+  }
 
   create() {
     if (this.form.invalid) {

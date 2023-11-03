cd6d60b06f8a22b8a5733f11cfa87bd3f67c23c5
Jean Santos <jeanwfsantos@gmail.com>
2023-11-03 13:57:35 +0000

    refact channel model

--- frontend/src/app/models/channel.ts
+++ frontend/src/app/models/channel.ts
@@ -1,15 +1,4 @@
-interface User {
-  sub: string;
-  email_verified: boolean;
-  updated_at: string;
-  nickname: string;
-  name: string;
-  given_name: string;
-  locale: string;
-  family_name: string;
-  picture: string;
-  email: string;
-}
+import { User } from './user';
 
 export interface Channel {
   createdAt: string;
